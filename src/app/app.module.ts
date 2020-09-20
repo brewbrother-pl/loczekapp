7c0cd1954b09a37e352205fdd7373d1bc885c039
brewbrother-pl <47082733+brewbrother-pl@users.noreply.github.com>
2020-09-20 14:25:16 +0200

    brew

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,8 @@ import {MatButtonModule} from '@angular/material/button';
 import {MatCheckboxModule} from '@angular/material/checkbox'; 
 import { FormsModule } from '@angular/forms';
 import {MatCardModule} from '@angular/material/card';
+import {MatToolbarModule} from '@angular/material/toolbar'; 
+import {MatIconModule} from '@angular/material/icon'; 
 
 @NgModule({
   declarations: [
@@ -18,7 +20,8 @@ import {MatCardModule} from '@angular/material/card';
     AppRoutingModule,MatButtonModule,MatCardModule,
     BrowserAnimationsModule,
     FormsModule,
-    MatCheckboxModule
+    MatCheckboxModule,
+    MatToolbarModule, MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
